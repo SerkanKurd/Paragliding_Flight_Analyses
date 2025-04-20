9927ab77c9c02d0666c3331f3b93f7faf5185a14
serkankurd <serkankurd@gmail.com>
2025-04-20 18:55:20 +0300

    Refactor flight data processing to skip already processed files and handle exceptions
    - Added a check to load existing processed flight data from CSV if available.
    - Modified the file processing loop to skip files that are already in the dataframe.
    - Implemented error handling during file processing to continue on exceptions.
    - Updated the output to reflect the number of processed rows and removed unnecessary map creation code.

--- prepare_flightdata.py
+++ prepare_flightdata.py
@@ -109,21 +109,39 @@ def prepare_data(filename):
 
 
 if __name__ == "__main__":
-    df = pd.DataFrame()
+    if os.path.exists(os.path.join("data", "flight_data_processed.csv")):
+        df = pd.read_csv(os.path.join(
+            "data", "flight_data_processed.csv"), low_memory=False)
+        df_files = df["filename"].tolist()
+    else:
+        df = pd.DataFrame()
+        df_files = []
+
     files = [f for f in os.listdir("flightlogs") if f.lower().endswith(".igc")]
-    for index in range(len(files)):
-        file = files[index]
-        print(f"Processing {index+1}/{len(files)} File: {file}")
-        df_prepare = prepare_data(file)
+    for file in files:
+        if file in df_files:
+            print(f"{file} already in dataframe")
+            files.pop(files.index(file))
+
+    for file in files:
+        print(f"Processing {files.index(file)+1}/{len(files)} File: {file}")
+        try:
+            df_prepare = prepare_data(file)
+        except Exception as e:
+            print("Error on", file, ":", e)
+            continue
         print(df_prepare.head())
         df_prepare["filename"] = file
         df = pd.concat([df_prepare, df], ignore_index=True)
-    df.to_csv(os.path.join("data", "flight_data_processed.csv"), index=False)
+        df.to_csv(os.path.join("data", "flight_data_processed.csv"),
+                  index=False)
+        # os.remove(os.path.join("flightlogs", file))
+
     print(f"Processed {len(df)} rows")
-    create_map(df[["latitude",
-                   "longitude",
-                   "climb_rate_m/s",
-                   "elapsed_time",
-                   "zone"]])
+    # create_map(df[["latitude",
+    #                "longitude",
+    #                "climb_rate_m/s",
+    #                "elapsed_time",
+    #                "zone"]])
+    # print("Map created")
     print("Data saved to flight_data_processed.csv")
-    print("Map created")
