ecf2506db05cf608e2d503d6d225d54165ed22a6
serkankurd <serkankurd@gmail.com>
2025-04-21 20:18:40 +0300

    Enhance data preparation in prepare_flightlog.py by adding glide ratio calculation, elapsed time, and zone classification; optimize bearing and distance calculations.

--- prepare_flightlog.py
+++ prepare_flightlog.py
@@ -1,8 +1,35 @@
 import pandas as pd
 import weather_data as getweatherdata
 from IGC_file_parse import getfile as igc
-import os
 from functools import lru_cache
+from geopy.distance import geodesic
+import math
+import os
+
+
+def calculate_bearing(row):
+    lat1 = math.radians(row["previous_latitude"])
+    lon1 = math.radians(row["previous_longitude"])
+    lat2 = math.radians(row["latitude"])
+    lon2 = math.radians(row["longitude"])
+
+    delta_lon = lon2 - lon1
+
+    x = math.sin(delta_lon) * math.cos(lat2)
+    y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
+                                           * math.cos(lat2) *
+                                           math.cos(delta_lon))
+
+    initial_bearing = math.atan2(x, y)
+    initial_bearing = math.degrees(initial_bearing)
+    compass_bearing = int((initial_bearing + 360) % 360)
+
+    return compass_bearing
+
+
+def calculate_distance(row):
+    return geodesic((row["previous_latitude"], row["previous_longitude"]),
+                    (row["latitude"], row["longitude"])).meters
 
 
 @lru_cache(maxsize=10000)
@@ -19,7 +46,35 @@ def get_weather_data(row):
 
 def prepare_data(filename):
     df = igc(os.path.join("flightlogs", filename), 1)
-    df.fillna(0, True)
+    df["previous_latitude"] = df["latitude"].shift(1)
+    df["previous_longitude"] = df["longitude"].shift(1)
+    df["distance_from_takeoff_m"] = df.apply(
+        lambda row: geodesic(
+            (row["latitude"], row["longitude"]),
+            (df["latitude"].iloc[0], df["longitude"].iloc[0])).meters, axis=1)
+    df.drop(df.index[0], inplace=True)
+    df["distance_m"] = df.apply(lambda row: geodesic(
+        (row["previous_latitude"], row["previous_longitude"]),
+        (row["latitude"], row["longitude"])).meters, axis=1)
+    df["speed_km/s"] = ((df["distance_m"]/1000) /
+                        (df["datetime"].diff().dt.total_seconds()/3600))
+    df["climb_m"] = df["gps_altitude_m"].diff()
+    df["climb_m(delta)"] = df["gps_altitude_m"].diff(20)
+    df["climb_rate_m/s"] = df["climb_m"] / \
+        df["datetime"].diff().dt.total_seconds()
+    df["bearing"] = df.apply(calculate_bearing, axis=1)
+    df["delta_bearing"] = abs((df["bearing"].diff() + 180) % 360 - 180)
+    df["glide_ratio"] = df.apply(
+        lambda row: row["distance_m"] /
+        abs(row["climb_m"]) if row["climb_m"] != 0 else 0,
+        axis=1)
+    df.fillna(0, inplace=True)
+    df["elapsed_time"] = (
+        df["datetime"] - df["datetime"].iloc[0]).dt.total_seconds()
+    df["zone"] = df.apply(lambda row: "thermal" if (
+        row["climb_m(delta)"] > 0 or
+        row["climb_rate_m/s"] > 0)
+        else "standart", axis=1)
     df[["temp",
         "pressure",
         "humidity",
@@ -27,6 +82,13 @@ def prepare_data(filename):
         "wind_speed",
         "wind_deg"]] = df.apply(get_weather_data, axis=1, result_type="expand")
 
+    # find and delete before take off
+    mask = df["speed_km/s"] > 10
+    if mask.any():
+        first_idx = mask.idxmax()
+        df = df.loc[first_idx:].reset_index(drop=True)
+    else:
+        df = df.iloc[0:0]
     return df
 
 
